bf18f9a50d22f8207830be10d3e29cd21cfb51e1
maksimus <maksimilianos>
2020-09-07 01:51:32 +0300

    completed

--- First task.js
+++ First task.js
@@ -1,9 +1,9 @@
 function first(x) {
     //
-    var arr =new Array(x)
-    var y=String()
+    let arr =new Array(x)
+    let y=String()
     //console.log("First task")
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         arr[i]=i+1
         y+=arr[i].toString()+" "
         //console.log(arr[i])
@@ -12,14 +12,11 @@ function first(x) {
 }
 function second(x,y,z)
 {
-    if(x%y===0&&x%z===0)return true
-    return false
+    return x % y === 0 && x % z === 0;
 }
 function third(x)
 {
-    return x=x[0].toUpperCase()+x.slice(1)
-
-    // console.log(x)
+    return x[0].toUpperCase()+x.slice(1)
 }
 console.log("First task: "+"arr(5)"+first(5))
 console.log("Second task: "+"11,3,4 => "+second(11,3,4))
