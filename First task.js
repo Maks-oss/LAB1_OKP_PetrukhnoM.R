9a9657c1b55daf45f3e21d0eaca37a3887e625ea
maksimus <maksimilianos>
2020-10-02 01:22:35 +0300

    # Conflicts: #	First task.js

--- First task.js
+++ First task.js
@@ -17,16 +17,7 @@ function third(x)
 {
     return x[0].toUpperCase()+x.slice(1)
 }
-function process()
-{
-    let x=document.getElementById('first').value
-    console.log(first(parseInt(x)))
-    let y=document.getElementById('second').value
-    y=y.split(",")
-    console.log(second(parseInt(y[0]),parseInt(y[1]),parseInt(y[2])))
-    let z=document.getElementById('third').value
-    console.log(third(z))
-}
-// console.log("First task: "+"arr(5)"+first(5))
-// console.log("Second task: "+"11,3,4 => "+second(11,3,4))
-// console.log("Third task: "+ "george => "+third("george"))
+
+console.log("First task: "+"arr(5)"+first(5))
+console.log("Second task: "+"11,3,4 => "+second(11,3,4))
+console.log("Third task: "+ "george => "+third("george"))
