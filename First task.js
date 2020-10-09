c579e61e157a383d4ca2ebb70a1a5a23c3ec4028
maksimus <maksimilianos>
2020-10-09 15:33:03 +0300

    Fix

--- First task.js
+++ First task.js
@@ -1,11 +1,7 @@
 function first(x) {
     let arr =new Array(x)
-    let y=String()
-    //console.log("First task")
     for (let i = 0; i < arr.length; i++) {
         arr[i]=i
-
-        //console.log(arr[i])
     }
     return arr
 }
@@ -18,6 +14,6 @@ function third(x)
     return x[0].toUpperCase()+x.slice(1)
 }
 
-console.log("First task: "+"arr(5)"+first(7))
+console.log("First task: "+"arr(5)"+first(5))
 console.log("Second task: "+"11,3,4 => "+second(11,3,4))
 console.log("Third task: "+ "george => "+third("george"))
